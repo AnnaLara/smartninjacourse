a7b82e28a9ac5ba30e8effdb8d9248d007905548
Anna Zubova <anna@leolog.com>
2018-03-28 14:29:19 +0200

    Updated property options

--- GuestBook/models.py
+++ GuestBook/models.py
@@ -1,7 +1,7 @@
 from google.appengine.ext import ndb
 
 class Message(ndb.Model):
-    name = ndb.StringProperty()
+    name = ndb.StringProperty(default = "Anonymous")
     email = ndb.StringProperty()
-    text = ndb.TextProperty()
+    text = ndb.TextProperty(required=True)
     created = ndb.DateTimeProperty(auto_now_add=True)
